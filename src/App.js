04025331e72bbc5933d96b0142d4cc6029854762
Malcolm Scerri <malcolm.scerri@xcaliber.com>
2019-01-31 17:05:46 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
+	    This was changed ->  Hello Malcolm! :))) Hihihih
           </p>
           <a
             className="App-link"
